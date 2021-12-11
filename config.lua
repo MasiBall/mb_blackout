dad04a4123fcede7de5dcfe63bb2bf9b31d9affa
MasiBall <76224425+MasiBall@users.noreply.github.com>
2021-12-11 12:28:34 +0200

    Update config.lua

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+-- General configuration
+Config.BlackoutDuration = 60
+Config.SendNotification = true
+Config.Soundeffect = true
+
+-- How blackout can be triggered
+Config.UsableItem = true -- Item can be changed in sv_bo.lua
+Config.UseCommand = true -- Command can be changed in sv_bo.lua
+
+-- Blackout notify
+Config.NotifyBlackoutON = 'CHAR_LJT'
+Config.NotifyBlackoutOFF = 'CHAR_ASHLEY' 
