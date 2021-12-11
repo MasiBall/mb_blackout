2a894507da901d1318a3678cef1c357a6dda308c
MasiBall <76224425+MasiBall@users.noreply.github.com>
2021-12-11 18:39:03 +0200

    client + server done

--- client/cl_bo.lua
+++ client/cl_bo.lua
@@ -1,1 +1,42 @@
+ESX = ESX
+local blackoutState = false
 
+if Config.UseOldESX then
+    ESX = nil
+    TriggerEvent('esx:getSharedObject', function(obj)
+        ESX = obj
+    end)
+end
+
+function AlertSound()
+    PlaySoundFrontend(-1, "Out_Of_Bounds_Timer", "DLC_HEISTS_GENERAL_FRONTEND_SOUNDS", 1)
+end
+
+RegisterNetEvent('mb_blackout:triggerblackout')
+AddEventHandler('mb_blackout:triggerblackout', function()
+    BlackoutOn()
+    Citizen.Wait(Config.BlackoutDuration *1000)
+    BlackoutOff()
+end)
+
+function BlackoutOn()
+    SetArtificialLightsState(true)
+    if Config.ShowVehicleLights then
+        SetArtificialLightsStateAffectsVehicles(false)
+    end
+    ESX.ShowAdvancedNotification('HACKED', '', 'Time to fear the greatness of Masi', Config.NotifyBlackoutON, 5, false)
+end
+
+function BlackoutOff()
+    SetArtificialLightsState(false)
+    if Config.ShowVehicleLights then
+        SetArtificialLightsStateAffectsVehicles(true)
+    end
+    ESX.ShowAdvancedNotification('GOVERNMENT', '', 'Power management system has been restored', Config.NotifyBlackoutOFF, 5, false)
+end
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource == GetCurrentResourceName() then
+        BlackoutOff()
+    end
+end)
--- server/sv_bo.lua
+++ server/sv_bo.lua
@@ -1,4 +1,57 @@
 -- Configuration
 local blackoutCommand = 'hackbo'
-local blackoutItem = 'EMP launcher'
+local blackoutItem = 'laptop'
 local discordwebhook = 'INSERT_WEBHOOK_HERE'
+
+if Config.UseOldESX then
+    ESX = nil
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+end
+
+if Config.UseCommand then
+RegisterCommand(blackoutCommand, function(source, args, rawCommand)
+    local source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local itemCount = xPlayer.getInventoryItem(blackoutItem).count
+    if itemCount >= 1 then
+        xPlayer.removeInventoryItem(blackoutItem, 1)
+        TriggerEvent('mb_blackout:server:triggerblackout', source)
+        end
+    end, false)
+end
+
+if Config.UsableItem then
+    ESX.RegisterUsableItem(blackoutItem, function(source)
+        local source = source
+        local xPlayer = ESX.GetPlayerFromId(source)
+
+        xPlayer.removeInventoryItem(blackoutItem, 1)
+        TriggerEvent('mb_blackout:server:triggerblackout', source)
+    end)
+end
+
+RegisterServerEvent('mb_blackout:server:triggerblackout')
+AddEventHandler('mb_blackout:server:triggerblackout', function(source)
+	local source = source
+        if Config.DiscordLog then
+            sendToDiscordLogsEmbed(3158326, '`💀` | Blackout',' Player: `' ..GetPlayerName(source).. '` - `'..GetPlayerIdentifier(source, 0)..'` triggered blackout')
+        end
+		TriggerClientEvent('mb_blackout:triggerblackout', -1)
+	end
+end)
+
+function sendToDiscordLogsEmbed(color, name, message, footer)
+    local footer = 'Made By MasiBall - '..os.date("%d/%m/%Y - %X")
+    local embed = {
+          {
+              ["color"] = color,
+              ["title"] = "**"..name.."**",
+              ["description"] = message,
+              ["footer"] = {
+              ["text"] = footer,
+              },
+          }
+      }
+  
+    PerformHttpRequest(discordwebhook, function(err, text, headers) end, 'POST', json.encode({username = 'Logs', embeds = embed}), { ['Content-Type'] = 'application/json' })
+end
